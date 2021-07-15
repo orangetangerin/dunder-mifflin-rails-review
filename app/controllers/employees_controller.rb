ec4bd2c5e1ce7bc75df106d710f0ca0d5dc5d488
Jonathan Yu <jonyu02@gmail.com>
2021-07-15 00:37:56 -0400

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,43 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def create
+        @employee = Employee.new(strong_params)
+
+        if @employee.valid?
+            @employee.save
+            redirect_to @employee
+        else
+            flash[:errors] = @employee.errors.full_messages
+            redirect_to new_employee_path
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+        @dogs = Dog.all
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(strong_params)
+        redirect_to @employee
+    end
+
+    private
+
+    def strong_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
 end
