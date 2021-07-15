637ebedaa0c4a5df32f6c2fd5668a4c003460acf
Jonathan Yu <jonyu02@gmail.com>
2021-07-15 00:36:56 -0400

    commit tree b8a63512d80d5c8737619e937bba8aba1f0c9bbf

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    
+    validates :alias, :title , uniqueness: true
+
+    def name
+        "#{self.first_name} #{self.last_name}"
+    end
 end
